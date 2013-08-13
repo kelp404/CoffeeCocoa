02fecaff22f0dfa370f494c952ff2a1fb00ff028
Kelp <kelp@phate.org>
2013-08-14 05:37:28 +0800

    added testAddAndRemoveHandler and testCleanupString at CocoaInCoffeeTest.

--- CoffeeCocoa/CoffeeCocoaTests/CocoaInCoffeeTest.m
+++ CoffeeCocoa/CoffeeCocoaTests/CocoaInCoffeeTest.m
@@ -25,4 +25,45 @@
     STAssertEqualObjects(_cic.webView, _webView, nil);
 }
 
+- (void)testAddAndRemoveHandler
+{
+    NSNumber *handlerId = [_cic addHandler:^id(id object) {
+        return @YES;
+    }];
+    STAssertEqualObjects(handlerId, @0, nil);
+    [_cic removeHandlerById:handlerId];
+    
+    handlerId = [_cic addHandler:^id(id object) {
+        return @NO;
+    }];
+    STAssertEqualObjects(handlerId, @1, nil);
+    [_cic removeHandlerById:handlerId];
+}
+
+- (void)testCleanupString
+{
+    NSString *output = cleanupString(nil);
+    STAssertNil(output, @"test nil");
+    
+    output = cleanupString(@"");
+    STAssertEqualObjects(output, @"", @"test empty string");
+    
+    output = cleanupString(@"\n");
+    STAssertEqualObjects(output, @"\\n", @"test new line char");
+    
+    output = cleanupString(@"\\");
+    STAssertEqualObjects(output, @"\\\\", nil);
+    
+    output = cleanupString(@"\"");
+    STAssertEqualObjects(output, @"\\\"", nil);
+    
+    NSString *javaScript = @"function() {\n"
+    "return \"\t\";\n"
+    "}";
+    output = cleanupString(javaScript);
+    STAssertEqualObjects(output, @"function() {\\nreturn \\\"\t\\\";\\n}", nil);
+}
+
+
+
 @end
